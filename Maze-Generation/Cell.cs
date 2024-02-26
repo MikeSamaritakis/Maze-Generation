52074d35ac2880e2c6caca9135baba2a43d030bc
Michail Samaritakis <mikesamaritakis@gmail.com>
2024-02-26 21:11:18 +0100

    trying to convert python to the VS project

---
+++ Maze-Generation/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Generation
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool Visited { get; set; } = false;
+        // Walls: Top, Right, Bottom, Left
+        public bool[] Walls { get; set; } = { true, true, true, true };
+
+        public Cell(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        // Add methods for cell logic here (e.g., CheckNeighbors)
+    }
+}
--- Maze-Generation/Form1.Designer.cs
+++ Maze-Generation/Form1.Designer.cs
@@ -1,5 +1,80 @@
-﻿namespace Maze_Generation
+﻿using Maze_Generation;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Maze_Generation
 {
+
+
+    public partial class Form1 : Form
+    {
+        private List<Cell> cells = new List<Cell>();
+        private int cols, rows;
+        private int cellSize = 20; // Change as needed
+        private Cell currentCell;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMaze();
+        }
+
+        private void InitializeMaze()
+        {
+            cols = mazePanel.Width / cellSize;
+            rows = mazePanel.Height / cellSize;
+
+            for (int j = 0; j < rows; j++)
+            {
+                for (int i = 0; i < cols; i++)
+                {
+                    cells.Add(new Cell(j, i));
+                }
+            }
+
+            currentCell = cells[0];
+            currentCell.Visited = true;
+
+            // Set up Panel
+            mazePanel.Paint += new PaintEventHandler(Draw);
+            mazePanel.Invalidate(); // Causes the panel to redraw
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.Clear(Color.White);
+
+            // Draw maze here
+            foreach (var cell in cells)
+            {
+                DrawCell(cell, g);
+            }
+        }
+
+        private void DrawCell(Cell cell, Graphics g)
+        {
+            int x = cell.Col * cellSize;
+            int y = cell.Row * cellSize;
+            Pen pen = new Pen(Color.Black, 2);
+
+            // Top
+            if (cell.Walls[0]) g.DrawLine(pen, x, y, x + cellSize, y);
+            // Right
+            if (cell.Walls[1]) g.DrawLine(pen, x + cellSize, y, x + cellSize, y + cellSize);
+            // Bottom
+            if (cell.Walls[2]) g.DrawLine(pen, x, y + cellSize, x + cellSize, y + cellSize);
+            // Left
+            if (cell.Walls[3]) g.DrawLine(pen, x, y, x, y + cellSize);
+        }
+
+        // Implement maze generation logic here (similar to Python version)
+        // You might need to adapt it for C# and Windows Forms specifics
+    }
+
+
     partial class Form1
     {
         /// <summary>
@@ -28,13 +103,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mazePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mazePanel
+            // 
+            this.mazePanel.Location = new System.Drawing.Point(0, -1);
+            this.mazePanel.Name = "mazePanel";
+            this.mazePanel.Size = new System.Drawing.Size(800, 451);
+            this.mazePanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mazePanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mazePanel;
     }
 }
 
